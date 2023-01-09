72c01eb9556086fce56721edeae8aa53125a2c14
kasutu <jeromecabugwason2@protonmail.com>
2023-01-09 20:57:35 +0800

    url: Refactor return type
    updated return type to url list

--- src/main/java/com/splitscale/shield/endpoints/url/read/ReadUrlEndpoint.java
+++ src/main/java/com/splitscale/shield/endpoints/url/read/ReadUrlEndpoint.java
@@ -2,9 +2,11 @@ package com.splitscale.shield.endpoints.url.read;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.splitscale.fordastore.core.url.Url;
+import com.splitscale.fordastore.core.url.UrlResponse;
 import com.splitscale.fordastore.core.url.read.ReadUrlInteractor;
 import com.splitscale.shield.jws.ShieldJws;
 
@@ -15,11 +17,22 @@ public class ReadUrlEndpoint {
     this.readUrlInteractor = readUrlInteractor;
   }
 
-  public List<Url> getALLByContainerID(Long containerId, String jwsToken) throws IOException, GeneralSecurityException {
-
+  public List<UrlResponse> getALLByContainerID(Long containerId, String jwsToken)
+      throws IOException, GeneralSecurityException {
     ShieldJws.validateJws(jwsToken);
 
-    return readUrlInteractor.getALLByContainerID(containerId);
+    List<UrlResponse> responses = new ArrayList<UrlResponse>();
+    List<Url> urls = readUrlInteractor.getALLByContainerID(containerId);
+
+    for (Url url : urls) {
+      UrlResponse urlResponse = new UrlResponse();
+      urlResponse.setUrlID(url.getUrlID());
+      urlResponse.setInnerUrl(url.getInnerUrl());
+      responses.add(urlResponse);
+    }
+
+    return responses;
+
   }
 
   public Url getByUrlID(Long urlId, String jwsToken) throws IOException, GeneralSecurityException {
