3922d7c73bdaff47c27badf6af66b48aa9afd408
Jerome Cabugwason <jeromecabugwason2@protonmail.com>
2023-01-09 21:22:24 +0800

    For-dev-environment (#15)
    * kws: Updated Props
    
    Description:
    updated props loader dir
    
    Expected behavior:
    load props in resources
    Co-authored-by: Aljason Javier <AljasonJavier@users.noreply.github.com>
    Co-authored-by: Yui187 <Yui187@users.noreply.github.com>
    
    * url: Add endpoint
    
    Description:
    added url endpoint cuz we forgot
    
    Expected behavior:
    able to get all urls via endpoint
    
    * url: Refactor return type
    
    updated return type to url list
    
    * url: Refactor return type
    
    Description:
    added return type for `getByUrlID`
    
    Expected behavior:
    now return UrlResponse instead of Url
    
    Co-authored-by: Aljason Javier <AljasonJavier@users.noreply.github.com>
    Co-authored-by: Yui187 <Yui187@users.noreply.github.com>

---
+++ src/main/java/com/splitscale/shield/endpoints/url/read/ReadUrlEndpoint.java
@@ -0,0 +1,50 @@
+package com.splitscale.shield.endpoints.url.read;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.splitscale.fordastore.core.url.Url;
+import com.splitscale.fordastore.core.url.UrlResponse;
+import com.splitscale.fordastore.core.url.read.ReadUrlInteractor;
+import com.splitscale.shield.jws.ShieldJws;
+
+public class ReadUrlEndpoint {
+  private ReadUrlInteractor readUrlInteractor;
+
+  public ReadUrlEndpoint(ReadUrlInteractor readUrlInteractor) {
+    this.readUrlInteractor = readUrlInteractor;
+  }
+
+  public List<UrlResponse> getALLByContainerID(Long containerId, String jwsToken)
+      throws IOException, GeneralSecurityException {
+    ShieldJws.validateJws(jwsToken);
+
+    List<UrlResponse> responses = new ArrayList<UrlResponse>();
+    List<Url> urls = readUrlInteractor.getALLByContainerID(containerId);
+
+    for (Url url : urls) {
+      UrlResponse urlResponse = new UrlResponse();
+      urlResponse.setUrlID(url.getUrlID());
+      urlResponse.setInnerUrl(url.getInnerUrl());
+      responses.add(urlResponse);
+    }
+
+    return responses;
+
+  }
+
+  public UrlResponse getByUrlID(Long urlId, String jwsToken) throws IOException, GeneralSecurityException {
+
+    ShieldJws.validateJws(jwsToken);
+
+    Url url = readUrlInteractor.getByUrlID(urlId);
+
+    UrlResponse urlResponse = new UrlResponse();
+    urlResponse.setUrlID(url.getUrlID());
+    urlResponse.setInnerUrl(url.getInnerUrl());
+
+    return urlResponse;
+  }
+}
