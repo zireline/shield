a5a441e6a447364745e8795d56b9274a5884ab15
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-11-13 11:28:56 +0800

    jwt: Add refresh token test
    Description:
    provide basic tests for the refresh token service
    
    Expected behavior:
    should generate a refresh token and validate it

---
+++ src/test/java/com/splitscale/shield/RefreshTokenManagerTest.java
@@ -0,0 +1,34 @@
+package com.splitscale.shield;
+
+import org.junit.jupiter.api.Test;
+
+import com.splitscale.shield.refreshtoken.InMemoryTokenStorage;
+import com.splitscale.shield.refreshtoken.RefreshTokenManager;
+import com.splitscale.shield.refreshtoken.TokenStorage;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RefreshTokenManagerTest {
+
+  @Test
+  void generateAndValidateRefreshToken() {
+    // Example usage with InMemoryTokenStorage
+    TokenStorage inMemoryTokenStorage = new InMemoryTokenStorage();
+    RefreshTokenManager refreshTokenManager = new RefreshTokenManager(inMemoryTokenStorage);
+    TimeUnit days = TimeUnit.DAYS;
+    String uid = "some-id";
+
+    // Generate refresh token for user with ID "some-id"
+    String generatedToken = refreshTokenManager.generateRefreshToken(uid, 30, days); // 24 hours
+    System.out.println("Generated Refresh Token: " + generatedToken);
+
+    // Validate the refresh token for user with ID uid
+    boolean isValidToken = refreshTokenManager.validateRefreshToken(generatedToken, uid);
+    assertTrue(isValidToken, "Refresh token should be valid");
+
+    // Validate an invalid refresh token
+    boolean isInvalidToken = refreshTokenManager.validateRefreshToken("invalid_token", uid);
+    assertFalse(isInvalidToken, "Refresh token should be invalid");
+  }
+}
